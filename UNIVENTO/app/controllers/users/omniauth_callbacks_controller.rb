914fbe51d00d520f989a3c974a153c2337a81240
SofiaReis <sofiareis1994@gmail.com>
2015-11-11 11:44:52 +0000

    add favicon

--- UNIVENTO/app/controllers/users/omniauth_callbacks_controller.rb
+++ UNIVENTO/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def facebook
 	    @user = User.from_omniauth(request.env["omniauth.auth"])
 	    
