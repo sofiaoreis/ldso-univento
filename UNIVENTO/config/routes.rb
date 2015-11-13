05ee63e17614685d3d03325a01199f661dd36c5e
leonelpeixoto <leonelpeixoto@gmail.com>
2015-11-13 00:20:28 +0000

    create a promoter

--- UNIVENTO/config/routes.rb
+++ UNIVENTO/config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :event
 
+  resources :promoter
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :users do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_session
