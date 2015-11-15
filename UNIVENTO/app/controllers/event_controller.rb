3c4030f4a5e4947e15bf53857c2ce3bc6e1b3ce7
leonelpeixoto <leonelpeixoto@gmail.com>
2015-11-15 23:54:19 +0000

    List of upcoming events (incomplete)

--- UNIVENTO/app/controllers/event_controller.rb
+++ UNIVENTO/app/controllers/event_controller.rb
@@ -1,7 +1,19 @@
 class EventController < ApplicationController
+
+# ========================================================
+
 	def show
     	@event = Event.find(params[:id])
     	#render plain: @event.eventtags.tags.inspect
     	#@event.eventtags.to_ary().each
  	end
+
+# ========================================================
+
+	def index
+    	@events = Event.all
+ 	end
+
+# ========================================================
+
 end
