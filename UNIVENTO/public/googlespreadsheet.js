625e7c1982e44759ea61bdd89ce45525d08af2a6
miguelnunes94 <miguelnunes_1994@hotmail.com>
2015-12-01 10:57:36 +0000

    script para obter e inserir dados na spreadsheet

--- UNIVENTO/public/googlespreadsheet.js
+++ UNIVENTO/public/googlespreadsheet.js
@@ -1,14 +1,89 @@
+var scriptLink = "https://script.google.com/macros/s/AKfycbxpb6ghoFH5Hyllkaq6DbmEepHU-nizHO6ukrOcJgLX4BRCAmM/exec";
 function start(){
 
-   $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbxpb6ghoFH5Hyllkaq6DbmEepHU-nizHO6ukrOcJgLX4BRCAmM/exec",
+	$("#carregarDados").on("click",getData);
+	
+	$('#enviaDados').submit(function(e) {
+		var inputs = $( this ).serializeArray();
+    	var values = [];
+    	values.push(getDateTime());
+    	$.each(inputs, function (i, input) {
+    		values.push(input.value);
+    	});
+    	sendData(values);
+    	e.preventDefault();
+	});  
+}
+
+
+function getData(){
+/*
+    https://docs.google.com/spreadsheets/d/1E6aAOkT-OU2R7MAN0T7v7EBZKuHvQMHYQIUdCDYIZgA/edit#gid=2062161593
+*/
+	$.ajax({
+        url: scriptLink,
     	type: "get",
     	dataType: "json",
-    	data: {nome: "nomeEnviado"}
+    	data: {
+    		funcao: "getData",
+    		spreadsheet: "https://docs.google.com/spreadsheets/d/1E6aAOkT-OU2R7MAN0T7v7EBZKuHvQMHYQIUdCDYIZgA/edit#gid=2062161593"
+    	}
     }).done(function (data) {
-      	console.log(data);
+      	$("#spreadsheet").html("");
+      	var table = "<table class=\"table\">";
+      	for (var i = 0; i < data.length; i++) {
+      		table+="<tr>";
+      		for (var k = 0; k < data[i].length; k++) {
+      			table+="<td>"+data[i][k]+"</td>";
+      		};
+      		table+="</tr>";
+      	};
+      	table+="</table";
+      	$("#spreadsheet").append(table);
     });
 }
 
+function sendData(values){
+	$.ajax({
+        url: scriptLink,
+    	type: "get",
+    	crossDomain: true,
+    	dataType: 'jsonp',
+    	data: {
+    		funcao: "sendData",
+    		dados: JSON.stringify(values),
+    		spreadsheet: "https://docs.google.com/spreadsheets/d/1E6aAOkT-OU2R7MAN0T7v7EBZKuHvQMHYQIUdCDYIZgA/edit#gid=2062161593"
+    	}
+    });
+    window.alert("Inscrição submetida");
+    getData();
+}
+
+function getDateTime() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(month.toString().length == 1) {
+        var month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+        var day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+        var hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+        var minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+        var second = '0'+second;
+    }   
+    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+     return dateTime;
+}
 
 $(start);
