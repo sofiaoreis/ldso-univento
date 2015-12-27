a8df7ac9f3bafe11064215965cb491af95268442
miguelnunes94 <miguelnunes_1994@hotmail.com>
2015-12-27 18:53:19 +0000

    aceitar convite

--- UNIVENTO/app/controllers/application_controller.rb
+++ UNIVENTO/app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
 	end
 #=end
 	def after_sign_in_path_for(resource)
+
+      if params[:colaborator].present? && params[:email].present?
+        if ConviteColaborator.accept(params[:colaborator], params[:email])
+          flash[:sucess] = "Convite aceite"
+        else
+          flash[:alert] = "Erro ao aceitar o convite"
+        end
+      end
     	session[:normal],session[:promoter] = User.getUserType(resource)
     	root_path
   	end
--- UNIVENTO/app/models/convite_colaborator.rb
+++ UNIVENTO/app/models/convite_colaborator.rb
@@ -3,4 +3,28 @@ class ConviteColaborator < ActiveRecord::Base
     self.primary_key = "hashID"
     has_one :promoter, :class_name => 'Promoter', :foreign_key => :promoterID
 
+    def self.accept(hashID,email)
+    	convite = ConviteColaborator.find_by_hashID(hashID)
+    	puts "===================================="
+		puts convite.inspect   
+		puts "===================================="
+    	if convite.present?
+    		if convite.email == email
+    			user = User.where(email: email).take
+    			if user.present?
+	    			normal = Normal.find_by_normalID(user.userID)
+	    			if normal.present?
+	    				colaborator = Colaborator.new
+	    				colaborator.normalID = normal.normalID
+    					colaborator.promoterID = convite.promoterID
+    					if colaborator.save
+    						convite.destroy
+    						return true
+    					end
+	    			end
+			    end
+    		end
+    	end	
+    	return false
+    end
 end
