997624068e942881399c930c86d52f87b8b3ed12
miguelnunes94 <miguelnunes_1994@hotmail.com>
2016-01-03 02:23:31 +0000

    bug resolvido

--- UNIVENTO/app/models/normal.rb
+++ UNIVENTO/app/models/normal.rb
@@ -1,7 +1,6 @@
 class Normal < ActiveRecord::Base
     self.table_name = 'Normal'
     self.primary_key = :normalID
-    ratyrate_rater
     
     has_many :colaborator, :class_name => 'Colaborator', :foreign_key => :normalID
     belongs_to :user, :class_name => 'User', :foreign_key => :normalID
